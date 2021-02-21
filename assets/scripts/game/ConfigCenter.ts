a5d42bac837df9a27299f3f29b2d4e384b29795c
DESKTOP-UH0M1SL\86134 <2662774600@qq.com>
2021-02-21 18:56:48 +0800

    实现渲染元素的控制

--- assets/scripts/game/ConfigCenter.ts
+++ assets/scripts/game/ConfigCenter.ts
@@ -11,6 +11,9 @@ namespace configCenter {
         b2WorldCreator: () => b2World
     }
 
+    /**
+     * 示例的合集
+     */
     export const examArr: BtnRec[] = [
         {
             info: `Sparky`,
@@ -94,19 +97,66 @@ namespace configCenter {
         }
     ]
 
-    
+    /**
+     * 绘制形状的标签
+     */
+    export const DRAW_SHAPE = 2**0;
+
+    /**
+     * 绘制碰撞盒的标签
+     */
+    export const DRAW_AABB = 2**1;
+
+    /**
+     * 画面元素单元的信息结构
+     */
     export interface CheckBoxRec {
+        /**
+         * 信息
+         */
         info: string;
+        /**
+         * 标签
+         */
+        tag: number;
     }
 
+    /**
+     * 画面元素的集合
+     */
     export const checkBoxData: CheckBoxRec[] = [
         {
-            info: `Shape`
+            info: `Shape`,
+            tag: DRAW_SHAPE
         },
         {
-            info: `AABB`
+            info: `AABB`,
+            tag: DRAW_AABB
         }
     ]
+
+    /**
+     * 颜色
+     */
+    export const color = {
+        shape: {
+            /**
+             * 外轮廓
+             */
+            outline: new cc.Color(255, 255, 255, 255),
+            /**
+             * 身体
+             */
+            body: new cc.Color(255, 255, 255, 100)
+        },
+
+        aabb: new cc.Color(0,0,255, 255)
+    }
+
+    /**
+     * b2 的形状缩放
+     */
+    export const b2ShapeScale = 20;
 }
 
 export default configCenter;
--- assets/scripts/game/component/Box2DDrawer.ts
+++ assets/scripts/game/component/Box2DDrawer.ts
@@ -7,7 +7,7 @@
 
 import EventerNoArgs from "../../frame/basic/EventerNoArgs";
 import b2Extend from "../../lib/b2_extend/B2Extend";
-import { b2AABB, b2Body, b2Color, b2Draw, b2DrawFlags, b2Fixture, b2Mat33, b2PolygonShape, b2Transform, b2Vec2, b2Vec3, b2World, b2_pi } from "../../lib/box2d_ts/Box2D";
+import { b2AABB, b2Body, b2BodyDef, b2Color, b2Draw, b2DrawFlags, b2Fixture, b2Mat33, b2PolygonShape, b2Transform, b2Vec2, b2Vec3, b2World, b2_pi } from "../../lib/box2d_ts/Box2D";
 import configCenter from "../ConfigCenter";
 import CheckBox from "./CheckBox";
 import dataCenter from "../DataCenter";
@@ -57,6 +57,14 @@ export default class Box2DDrawer extends cc.Component {
         Box2DDrawer.inst = this;
     }
 
+    /**
+     * 当前的绘制内容的标签
+     */
+    private _currDrawTag: number;
+
+    /**
+     * 正在操纵的世界
+     */
     private _b2w: b2World;
 
     public DrawB2World (b2w: b2World) {
@@ -67,26 +75,74 @@ export default class Box2DDrawer extends cc.Component {
             return;
         };
 
+        this._currDrawTag = 0;
+        for (let key in dataCenter.vo.enableRec) {
+            let prop = dataCenter.vo.enableRec[key];
+            let num = Number.parseInt(key);
+            if (!isNaN(num)) {
+                let cfg = configCenter.checkBoxData[num];
+                if (cfg != null) {
+                    if (!!prop) {
+                        this._currDrawTag |= cfg.tag;
+                    };
+                };
+            };
+        };
+
         for (let b = this._b2w.GetBodyList(); b; b = b.GetNext()) {
             let trans = b.GetTransform();
             let mat = b2Extend.GetTransMat(trans);
             for (let f = b.GetFixtureList(); f; f = f.GetNext()) {
-                this.DrawShape(f, new cc.Color(255, 255, 255, 255), mat);
+                if (this._currDrawTag & configCenter.DRAW_AABB) {
+                    for (let i = 0; i < f.m_proxyCount; i++) {
+                        let proxy = f.m_proxies[i];
+                        let aabb = proxy.treeNode.aabb;
+                        this.DrawAABB(aabb);
+                    };
+                };
+                
+                if (this._currDrawTag & configCenter.DRAW_SHAPE) {
+                    this.DrawShape(f, mat);
+                };
             };
         };
     }
 
-    private DrawShape (fixt: b2Fixture, color: cc.Color, mat: b2Mat33) {
-        const shape = fixt.GetShape();
-        const poly = b2Extend.GetPolygonShape(shape);
-        this.DrawPolygonShape(poly, color, mat);
+    /**
+     * 绘制碰撞盒
+     * @param aabb 
+     */
+    private DrawAABB (aabb: b2AABB) {
+        this.GraphicsMoveTo(aabb.lowerBound.x, aabb.lowerBound.y);
+        this.GraphicsLineTo(aabb.upperBound.x, aabb.lowerBound.y);
+        this.GraphicsLineTo(aabb.upperBound.x, aabb.upperBound.y);
+        this.GraphicsLineTo(aabb.lowerBound.x, aabb.upperBound.y);
+        this.GraphicsLineTo(aabb.lowerBound.x, aabb.lowerBound.y);
+        this.graphics.strokeColor = configCenter.color.aabb;
+        this.graphics.stroke();
     }
 
-    private DrawPolygonShape (poly: b2PolygonShape, color: cc.Color, mat: b2Mat33) {
+    /**
+     * 绘制形状
+     * @param fixt 
+     * @param mat 
+     */
+    private DrawShape (fixt: b2Fixture, mat: b2Mat33) {
+        const shape = fixt.GetShape();
+        const poly = b2Extend.GetPolygonShape(shape);
+        this.DrawPolygonShape(poly, mat);
+    }
+
+    /**
+     * 绘制多边形
+     * @param poly 
+     * @param mat 
+     */
+    private DrawPolygonShape (poly: b2PolygonShape, mat: b2Mat33) {
         if (poly == null) {
             return;
         };
-        this.DrawSolidPolygon(poly.m_vertices, poly.m_count, color, mat);
+        this.DrawSolidPolygon(poly.m_vertices, poly.m_count, mat);
     }
 
     /**
@@ -105,8 +161,6 @@ export default class Box2DDrawer extends cc.Component {
         this._tempPos.z = 1;
 
         b2Mat33.MulM33V3(mat, this._tempPos, this._tempPos);
-        this._tempPos.x *= 20;
-        this._tempPos.y *= 20;
 
         return this._tempPos;
     }
@@ -118,17 +172,25 @@ export default class Box2DDrawer extends cc.Component {
      * @param color 
      * @param mat 
      */
-    private DrawSolidPolygon (vArr: b2Vec2[], vCount: number, color: cc.Color, mat: b2Mat33) {
+    private DrawSolidPolygon (vArr: b2Vec2[], vCount: number, mat: b2Mat33) {
         this.Transform(vArr[0], mat);
-        this.graphics.moveTo(this._tempPos.x, this._tempPos.y);
+        this.GraphicsMoveTo(this._tempPos.x, this._tempPos.y);
         for (let i = 1; i < vCount; i++) {
             this.Transform(vArr[i], mat);
-            this.graphics.lineTo(this._tempPos.x, this._tempPos.y);
+            this.GraphicsLineTo(this._tempPos.x, this._tempPos.y);
         };
         this.graphics.close();
-        this.graphics.fillColor = color.lerp(cc.Color.BLACK, 0.5);
+        this.graphics.fillColor = configCenter.color.shape.body;
         this.graphics.fill();
-        this.graphics.strokeColor = color;
+        this.graphics.strokeColor = configCenter.color.shape.outline;
         this.graphics.stroke();
+    }
+
+    private GraphicsMoveTo (x: number, y: number) {
+        this.graphics.moveTo(x * configCenter.b2ShapeScale, y * configCenter.b2ShapeScale);
+    }
+
+    private GraphicsLineTo (x: number, y: number) {
+        this.graphics.lineTo(x * configCenter.b2ShapeScale, y * configCenter.b2ShapeScale);
     }
 }
